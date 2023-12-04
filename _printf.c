fca1985c4b0be6fbdf9afba6a7862cac41d508c5
Favour <ofavourite001@gmail.com>
2023-12-04 13:59:37 +0100

    Edit 5

--- _printf.c
+++ _printf.c
@@ -1,11 +1,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ * 
+ * Return: number of characters printed
+ */
 int _printf(const char* format, ...) {
     int count;
 
-    va_list args;
-    va_start(args, format);
+    va_list input;
+    va_start(input, format);
 
     count = 0;
     while (*format != '\0') {
@@ -14,12 +20,12 @@ int _printf(const char* format, ...) {
             format++;
             switch (*format) {
                 case 's': {
-                    char* str = va_arg(args, char*);
-                    printf("%s", str);
+                    char* st = va_arg(input, char*);
+                    printf("%s", st);
                     break;
                 }
                 case 'c': {
-                    char ch = va_arg(args, int);
+                    char ch = va_arg(input, int);
                     printf("%c", ch);
                     break;
                 }
@@ -33,7 +39,7 @@ int _printf(const char* format, ...) {
         format++;
     }
 
-    va_end(args);
+    va_end(input);
 
     return(count);
 }
