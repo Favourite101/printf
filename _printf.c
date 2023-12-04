9951076ef926206083733ae245c237ee2959c69c
Favour <ofavourite001@gmail.com>
2023-12-04 14:14:51 +0100

    Edit 6

--- _printf.c
+++ _printf.c
@@ -4,42 +4,52 @@
 /**
  * _printf - prints anything
  * @format: list of argument types passed to the function
- * 
+ *
  * Return: number of characters printed
  */
-int _printf(const char* format, ...) {
-    int count;
+int _printf(const char* format, ...)
+{
+	int count;
+	char ch;
+	char *st;
 
-    va_list input;
-    va_start(input, format);
+	va_list input;
+	va_start(input, format);
 
-    count = 0;
-    while (*format != '\0') {
-        count++;
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 's': {
-                    char* st = va_arg(input, char*);
-                    printf("%s", st);
-                    break;
-                }
-                case 'c': {
-                    char ch = va_arg(input, int);
-                    printf("%c", ch);
-                    break;
-                }
-                default:
-                    putchar(*format);
-            }
-        } else {
-            putchar(*format);
-        }
+	count = 0;
+	while (*format != '\0')
+	{
+		count++;
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 's':
+				{
+					st = va_arg(input, char*);
+					printf("%s", st);
+					break;
+				}
+				case 'c':
+				{
+					ch = va_arg(input, int);
+					printf("%c", ch);
+					break;
+				}
+				default:
+					putchar(*format);
+			}
+		}
+		else
+		{
+			putchar(*format);
+		}
 
-        format++;
-    }
+		format++;
+	}
 
-    va_end(input);
+	va_end(input);
 
-    return(count);
+	return(count);
 }
